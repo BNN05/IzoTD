d3d6fa9bf0304e7763f015ddc561a1e98d64df72
ben051101@gmail.com <ben051101@gmail.com>
2022-04-26 20:42:52 +0200

    First real commit Created scene + some script

---
+++ Assets/Enemy.cs
@@ -0,0 +1,61 @@
+
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float speed = 10f;
+    public float startHealth;
+    public float health = 150;
+    public int worth = 50;
+    private Transform target;
+    private int wavepointIndex = 0;
+    void Start()
+    {
+        target = Waypoints.points[0];
+        health = startHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        //PlayerStats.Money += worth;
+        Destroy(gameObject);
+    }
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+        transform.rotation = target.rotation;
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.5f)
+        {
+            GetNextWaypoints();
+        }
+    }
+
+    void GetNextWaypoints()
+    {
+            if (wavepointIndex >= Waypoints.points.Length - 1)
+            {
+                EndPath(1);
+                return;
+            }
+            wavepointIndex++;
+            target = Waypoints.points[wavepointIndex];
+    }
+
+    void EndPath(int dif)
+    {
+        PlayerStats.Lives--;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+    public float timeBetweenWaves = 5f;
+    private float countdown = 5f;
+    private int waveIndex = 0;
+    public int difficulty;
+    public static int enemyCount = 0;
+
+    void Update()
+    {
+        if (countdown <= 0f && enemyCount == 0)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+        if (countdown > 0 && enemyCount == 0)
+        {
+            countdown -= Time.deltaTime;
+        }
+    }
+    public void killEnemy()
+    {
+        enemyCount--;
+    }
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+        PlayerStats.Rounds++;
+        for (int i = 0; i < waveIndex; i++)
+        {
+            if (waveIndex > 5)
+                waveIndex = 5;
+            SpawnEnemy();
+
+            yield return new WaitForSeconds(0.5f);
+        }
+
+
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+        enemyCount++;
+    }
+}

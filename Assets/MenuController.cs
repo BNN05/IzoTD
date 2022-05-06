4c4aea4e94ceb26e99d0819604f2393e6540f7f4
ben051101@gmail.com <ben051101@gmail.com>
2022-05-06 15:29:09 +0200

    Update in MainMenu

--- Assets/MenuController.cs
+++ Assets/MenuController.cs
@@ -6,11 +6,7 @@ using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
-    public GameObject button;
-    private void Start()
-    {
-        GameObject.Find("buttonName").GetComponentInChildren<Text>().text = "LaunchGame";
-    }
+  
     public void launchGame(string level)
     {
         SceneManager.LoadScene(level);
